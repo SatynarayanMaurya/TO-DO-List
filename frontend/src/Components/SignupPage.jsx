0287b067dcbf2f767befbdab3c999692de33d8c2
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2024-09-23 02:11:27 +0530

    Update SignupPage.jsx
    changed

--- frontend/src/Components/SignupPage.jsx
+++ frontend/src/Components/SignupPage.jsx
@@ -11,7 +11,7 @@ import { changeLogin } from '../Redux/Slice/LoginSlice';
 
 function SignupPage() {
     
-    const url = "https://to-do-list-backend-sns6.onrender.com"
+    const url = "https://to-do-list-backend-wjyp.onrender.com"
     const dispatch = useDispatch();
     const {register, handleSubmit, formState:{errors,reset}} = useForm(
         {
