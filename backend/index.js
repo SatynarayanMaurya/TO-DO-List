ded670612823f0680e6e3cd1a669690018f918ae
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2024-09-23 10:43:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
     origin: url,
     optionsSuccessStatus: 200 // For legacy browsers
   };
-app.use(cors(options));
+app.use(cors(corsOptions));
 app.use("/api/v1",routes);
 
 
